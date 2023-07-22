da23d951f075cffd4d70b540f2b53e69d21d06ac
Yash Sharma <yash304sharma@gmail.com>
2023-07-23 01:23:06 +0530

    Update QueueReversal.java

--- QueueReversal.java
+++ QueueReversal.java
@@ -17,3 +17,42 @@ class GfG{
         return q;
     }
 }
+
+
+
+//OVERALL CODE WRITTEN BY ME IS 
+import java.util.*;
+
+class HelloWorld {
+    
+    public static void printt(Queue<Integer> q){
+        while(!q.isEmpty()){
+            int x=q.peek();
+            System.out.print(x+" ");
+            q.remove();
+        }
+    }
+    
+    public static void reversal(Queue<Integer> q){
+        Stack<Integer> st=new Stack<Integer>();
+        while(!q.isEmpty()){
+            st.push(q.peek());
+            q.remove();
+        }    
+        while(!st.isEmpty()){
+            q.add(st.peek());
+            st.pop();
+        }
+    }
+    
+    public static void main(String[] args) {
+        Queue<Integer> q=new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        //reversal(q);
+        printt(q);
+    }
+}

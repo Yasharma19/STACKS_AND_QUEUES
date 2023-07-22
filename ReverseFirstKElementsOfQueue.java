9d3c2c35cb842f7db38a219656fab8de0eb38837
Yash Sharma <yash304sharma@gmail.com>
2023-07-23 01:35:29 +0530

    Update ReverseFirstKElementsOfQueue.java

--- ReverseFirstKElementsOfQueue.java
+++ ReverseFirstKElementsOfQueue.java
@@ -19,6 +19,47 @@ class GfG {
             q.add(num);
         }
         return q;
-        
+    }
+}
+
+
+
+
+//DONE BY MYSELF
+import java.util.*;
+class HelloWorld {
+    public static void printt(Queue<Integer> q){
+        while(!q.isEmpty()){
+            int x=q.peek();
+            System.out.print(x+" ");
+            q.remove();
+        }
+    }
+    public static void kreversal(Queue<Integer> q,int k){
+        Stack<Integer> st=new Stack<Integer>();   
+        for(int i=0;i<k;i++){
+            st.push(q.peek());
+            q.remove();
+        }
+        for(int i=0;i<k;i++){
+            q.add(st.peek());
+            st.pop();
+        }
+        int n=5;
+        for(int i=0;i<n-k;i++){
+            int x=q.peek();
+            q.remove();
+            q.add(x);
+        }
+    }
+    public static void main(String[] args) {
+        Queue<Integer> q=new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        kreversal(q,3);
+        printt(q);
     }
 }

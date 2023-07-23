656c3d70bbb136593bc7f9594e92a11813b69a56
Yash Sharma <yash304sharma@gmail.com>
2023-07-23 13:26:04 +0530

    Create ImplementStackUsingQueues.java

---
+++ ImplementStackUsingQueues.java
@@ -0,0 +1,26 @@
+class HelloWorld{
+    
+    public MyStack() {
+        Queue<Integer> queue = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        queue.add(x);
+        for(int i = 1; i < queue.size(); i++) {
+            queue.add(queue.peek());
+            queue.remove();
+        }
+    }
+    
+    public int pop() {
+        return queue.remove();
+    }
+    
+    public int top() {
+        return queue.peek();
+    }
+    
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
